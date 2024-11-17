5a488965ab8e887f8db2bb23f491511ca36b7325
KL_jiana <2436262516@qq.com>
2024-11-17 15:36:13 +0800

    translatable fix

--- src/main/java/com/teampotato/quiet/command/QuietCommand.java
+++ src/main/java/com/teampotato/quiet/command/QuietCommand.java
@@ -20,47 +20,49 @@ import java.util.Collection;
 public class QuietCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal("quiet").requires((sourceStack) -> sourceStack.hasPermission(0))
-                .then(Commands.argument("targets", EntityArgument.players()).executes(ctx -> {
-                                    return quietPlayer(ctx.getSource(), EntityArgument.getPlayers(ctx, "targets"), (byte) 3, 30);
-                                })
-                                .then(Commands.argument("time", TimeArgument.time()).executes(ctx -> {
-                                                    return quietPlayer(ctx.getSource(), EntityArgument.getPlayers(ctx, "targets"), (byte) 3, IntegerArgumentType.getInteger(ctx, "time"));
-                                                })
-                                                .then(Commands.literal("LOWEST").executes(ctx -> {
-                                                            return quietPlayer(ctx.getSource(), EntityArgument.getPlayers(ctx, "targets"), (byte) 1, IntegerArgumentType.getInteger(ctx, "time"));
-                                                        })
-                                                )
-                                                .then(Commands.literal("LOW").executes(ctx -> {
-                                                            return quietPlayer(ctx.getSource(), EntityArgument.getPlayers(ctx, "targets"), (byte) 2, IntegerArgumentType.getInteger(ctx, "time"));
-                                                        })
-                                                )
-                                                .then(Commands.literal("NORMAL").executes(ctx -> {
+                .then(Commands.literal("add")
+                        .then(Commands.argument("targets", EntityArgument.players()).executes(ctx -> {
+                                            return quietPlayer(ctx.getSource(), EntityArgument.getPlayers(ctx, "targets"), (byte) 3, 30);
+                                        })
+                                        .then(Commands.argument("time", TimeArgument.time()).executes(ctx -> {
                                                             return quietPlayer(ctx.getSource(), EntityArgument.getPlayers(ctx, "targets"), (byte) 3, IntegerArgumentType.getInteger(ctx, "time"));
-                                                        }).requires(commandSourceStack -> commandSourceStack.hasPermission(3))
-                                                )
-                                                .then(Commands.literal("HIGH").executes(ctx -> {
-                                                            return quietPlayer(ctx.getSource(), EntityArgument.getPlayers(ctx, "targets"), (byte) 4, IntegerArgumentType.getInteger(ctx, "time"));
-                                                        }).requires(commandSourceStack -> commandSourceStack.hasPermission(3))
-                                                )
-                                                .then(Commands.literal("HIGHEST").executes(ctx -> {
-                                                            return quietPlayer(ctx.getSource(), EntityArgument.getPlayers(ctx, "targets"), (byte) 5, IntegerArgumentType.getInteger(ctx, "time"));
-                                                        }).requires(commandSourceStack -> commandSourceStack.hasPermission(3))
-                                                )
-                                )
+                                                        })
+                                                        .then(Commands.literal("LOWEST").executes(ctx -> {
+                                                                    return quietPlayer(ctx.getSource(), EntityArgument.getPlayers(ctx, "targets"), (byte) 1, IntegerArgumentType.getInteger(ctx, "time"));
+                                                                })
+                                                        )
+                                                        .then(Commands.literal("LOW").executes(ctx -> {
+                                                                    return quietPlayer(ctx.getSource(), EntityArgument.getPlayers(ctx, "targets"), (byte) 2, IntegerArgumentType.getInteger(ctx, "time"));
+                                                                })
+                                                        )
+                                                        .then(Commands.literal("NORMAL").executes(ctx -> {
+                                                                    return quietPlayer(ctx.getSource(), EntityArgument.getPlayers(ctx, "targets"), (byte) 3, IntegerArgumentType.getInteger(ctx, "time"));
+                                                                }).requires(commandSourceStack -> commandSourceStack.hasPermission(3))
+                                                        )
+                                                        .then(Commands.literal("HIGH").executes(ctx -> {
+                                                                    return quietPlayer(ctx.getSource(), EntityArgument.getPlayers(ctx, "targets"), (byte) 4, IntegerArgumentType.getInteger(ctx, "time"));
+                                                                }).requires(commandSourceStack -> commandSourceStack.hasPermission(3))
+                                                        )
+                                                        .then(Commands.literal("HIGHEST").executes(ctx -> {
+                                                                    return quietPlayer(ctx.getSource(), EntityArgument.getPlayers(ctx, "targets"), (byte) 5, IntegerArgumentType.getInteger(ctx, "time"));
+                                                                }).requires(commandSourceStack -> commandSourceStack.hasPermission(3))
+                                                        )
+                                        )
+                        )
                 )
         );
     }
 
-    public static int quietPlayer(CommandSourceStack commandSource, Collection<ServerPlayer> players, byte strength, int time) {
+    public static int quietPlayer(CommandSourceStack commandSource, Collection<ServerPlayer> players, byte strength, float time) {
         ServerLevel serverLevel = commandSource.getLevel();
         players.forEach(serverPlayer -> {
-            QuietData quietData = new QuietData(serverPlayer.getScoreboardName(), serverPlayer.getUUID(), strength, serverLevel.getGameTime() + time);
+            QuietData quietData = new QuietData(serverPlayer.getScoreboardName(), serverPlayer.getUUID(), strength, (long) (serverLevel.getGameTime() + time));
             QuietDataUtil.addQuietData(quietData);
             NetworkHandler.CHANNEL.send(
                     PacketDistributor.PLAYER.with(() -> serverPlayer),
                     new QuietDataS2C(quietData)
             );
-            if (strength >= 3) serverPlayer.sendSystemMessage(Component.translatable("message.quiet.mute"));
+            if (strength >= 3) serverPlayer.sendSystemMessage(Component.translatable("message.quiet.mute", time / 20));
         });
         return 0;
     }
